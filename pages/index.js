7bf657f7bb07b8b087e08dfa0e4874dba30282ea
Steve Fabian <steve.fabian@gooddogs.com>
2022-08-11 09:44:07 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Hendersoville Bridge Center</title>
+        <title>Hendersonville Bridge Center</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Loading .. please wait" />
       </main>
 
       <Footer />
