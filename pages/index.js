fb968ab8b39ff29baef25cdac5e60051174aeccb
Steve Fabian <steve.fabian@gooddogs.com>
2022-08-11 11:57:27 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Script from 'next/script'
 
 export default function Home() {
   return (
